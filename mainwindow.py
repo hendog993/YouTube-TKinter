2ae707f0c37fbad1959d990f66f6125d694d6c88
Henry V. Gilbert <henrygilbert609@gmail.com>
2019-09-16 22:27:10 -0400

    Add files via upload

--- mainwindow.py
+++ mainwindow.py
@@ -5,7 +5,7 @@ _root = Tk()
 
 
 # TODO add executable functionality instead of just a script
-# TODO
+# TODO add path functionality to save and load methods
 
 def main():
     gui = Window(_root)
@@ -29,7 +29,7 @@ class Window:
         # File dropdown menu
         self.fileMenu = Menu(self.menu, tearoff=0)
         self.menu.add_cascade(label="File", menu=self.fileMenu)
-        self.fileMenu.add_command(label="Save", command=self.save_file)
+        self.fileMenu.add_command(label="Save", command=self.save_file_window)
         self.fileMenu.add_command(label="Open", command=self.open_file)
         self.fileMenu.add_command(label="Exit", command=self.exit)
 
@@ -57,10 +57,67 @@ class Window:
         self.helpMenu.add_command(label="Information", command=self.information_window)
         self.helpMenu.add_command(label="Tutorial", command=self.tutorial_window)
 
-    def save_file(self):
+    """
+    Method: save_file
+    Description: Opens a popup window that allows the user to save a file. Uses the open block to save
+                 and create a new file. 
+    TODO: if the file already exists, use w+ mode. 
+    Return: None 
+    """
+
+    def save_file_window(self):
         save_gui = Tk()
         save_gui.geometry('600x400')
+        file_contents = self.textspace.get(0.0, END)
+
+        def save_new_file(text=file_contents):
+            with open(file_name.get() + '.txt', 'w+') as file:
+                file.write(file_contents)
+                file.close()
+            return None
+
+        Label(save_gui, text="File Name: ").grid(row=0, column=0)
+        file_name = Entry(save_gui, width=40)
+        file_name.grid(row=0, column=1)
+
+        Label(save_gui, text="Directory: ").grid(row=1, column=0)
+        save_directory = Entry(save_gui, width=40)
+        save_directory.grid(row=1, column=1)
+
+        save_button = Button(save_gui, text="Save", command=save_new_file)
+        save_button.grid(row=1, column=3)
+        cancel_button = Button(save_gui, text="Cancel", command=save_gui.destroy)
+        cancel_button.grid(row=1, column=4)
+
         save_gui.mainloop()
+        return None
+
+    """
+    Method: open_file
+    Description: Loads a file based on the default path. Uses open block to insert text into main textbox.
+    Return: None 
+    """
+
+    def open_file(self):
+        load_gui = Tk()
+        load_gui.geometry('600x400')
+
+        def load_new_file():
+            with open(file_name.get() + '.txt', 'r') as file:
+                pass
+            # Write the file contents to the text box. Use insert method. Delete previous contents.
+            return None
+
+        Label(load_gui, text="File Name: ").grid(row=0, column=0)
+        file_name = Entry(load_gui, width=40)
+        file_name.grid(row=0, column=1)
+
+        save_button = Button(load_gui, text="Load", command=load_new_file)
+        save_button.grid(row=1, column=3)
+        cancel_button = Button(load_gui, text="Cancel", command=load_gui.destroy)
+        cancel_button.grid(row=1, column=4)
+
+        load_gui.mainloop()
         return None
 
     """
@@ -85,9 +142,6 @@ class Window:
         info_popup = Tk()
         info_popup.geometry('600x400')
         info_popup.mainloop()
-        pass
-
-    def open_file(self):
         pass
 
     def edit_undo(self):
